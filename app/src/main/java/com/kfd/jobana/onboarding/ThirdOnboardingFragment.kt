d6ed3409669f35d89bdad673742659f39d6a0a7c
by-westik <nastya.derevyanko.2020@mail.ru>
2023-04-16 22:09:32 +0300

    Add onboarding screen fragments

---
+++ app/src/main/java/com/kfd/jobana/onboarding/ThirdOnboardingFragment.kt
@@ -0,0 +1,19 @@
+package com.kfd.jobana.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kfd.jobana.R
+
+class ThirdOnboardingFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third_onboarding, container, false)
+    }
+
+}
