bbf4de1f9e189aea61c1fb890f03c6cea52594cc
by-westik <nastya.derevyanko.2020@mail.ru>
2023-05-12 00:05:33 +0300

    Add: dependency for datastore, userPreferences to save authToken

--- app/src/main/java/com/kfd/jobana/MainActivity.kt
+++ app/src/main/java/com/kfd/jobana/MainActivity.kt
@@ -3,7 +3,9 @@ package com.kfd.jobana
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.kfd.jobana.data.UserPreferences
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,6 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+
+        val userPreferences = UserPreferences(this)
+        Toast.makeText(this, "${userPreferences.authToken}", Toast.LENGTH_LONG).show()
         setContentView(R.layout.activity_main)
 
     }
---
+++ app/src/main/java/com/kfd/jobana/data/UserPreferences.kt
@@ -0,0 +1,37 @@
+package com.kfd.jobana.data
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class UserPreferences(
+    private val context: Context
+) {
+
+    companion object {
+        private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("user_data_store")
+        private val AUTH_KEY = stringPreferencesKey("auth_key")
+    }
+
+
+    val authToken: Flow<String?>
+        get() =  context.dataStore.data.map { preferences ->
+            preferences[AUTH_KEY]
+        }
+
+
+    suspend fun saveUserAuthToken(authToken: String) {
+        context.dataStore.edit {preferences ->
+            preferences[AUTH_KEY] = authToken
+
+        }
+    }
+
+
+}
